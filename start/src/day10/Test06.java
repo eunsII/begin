78788b0683ac05ccb45b94d87707e578696562ce
y <y@DESKTOP-1E4JF9F>
2022-03-08 18:21:01 +0900

    last

--- start/src/day10/Test06.java
+++ start/src/day10/Test06.java
@@ -5,6 +5,12 @@ public class Test06 {
 		System.out.println(getSum(1, 2));
 		System.out.println(getSum(1, 2, 3, 4, 5));
 		System.out.println(getSum(1, 2, 3,4,5,6,7,8,9));
+		
+		int[] arr = new int[20];
+		for(int i = 0 ; i < arr.length ; i++ ) {
+			arr[i] = (int)(Math.random()*20 + 1);
+		}
+		System.out.println(getSum(arr));
 	}
 	public static void main(String[] args) {
 		new Test06();
